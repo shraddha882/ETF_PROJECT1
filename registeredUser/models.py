2248bf760a9049d5d2ab20a30a37b5f5f3c46566
PrashantSGour <154967323+PrashantSGour@users.noreply.github.com>
2024-03-08 05:09:31 +0530

    all in one etf table template added

--- registeredUser/models.py
+++ registeredUser/models.py
@@ -6,6 +6,14 @@ from django.contrib.auth.models import User
 
 class RegisteredUser(models.Model):
 
+    Subscription_choices = [
+        ('Unsubscribed', 'Unsubscribed'),
+        ('Silver', 'Silver'),
+        ('Gold', 'Gold'),
+        ('Platinum', 'Platinum'),
+      
+    ]
+    
     username  = models.CharField(max_length = 255,null = False, unique = True,default = 'username')
     name = models.CharField(max_length=255, null  = False)
     email = models.EmailField(primary_key=True, null  = False)
@@ -15,11 +23,14 @@ class RegisteredUser(models.Model):
     login_status = models.BooleanField(default = False)
     is_verified = models.BooleanField(default = False)
     token = models.CharField(max_length=150, null =True)
+    sub_status = models.CharField(max_length =20, default='Unsubscribed',  choices=Subscription_choices)
     
 
 
     def __str__(self):
         return self.name
+    
+    
 class Wallet(models.Model):
     user = models.OneToOneField(RegisteredUser, on_delete=models.CASCADE)
     balance = models.DecimalField(max_digits=15, decimal_places=2, default=1000000.00)  # Initial balance of 10 lakhs
